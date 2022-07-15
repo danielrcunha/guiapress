7214a3637e4fd04fc77893e2ac5f5d8b29dfeafc
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2022-07-15 17:27:16 -0300

    commit
    commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//arquivo pricipal
 //importações
 const express = require("express");
 const app = express();
@@ -32,11 +33,57 @@ app.use("/", categoriesController);
 app.use("/", articlesController);
 
 app.get("/", (req, res) => {
-    Article.findAll().then(articles => {
-        res.render("index", { articles: articles });//enviando p/ frontend
-    });
+    Article.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then(articles => {
 
+        Category.findAll().then(categories => {
+            res.render("index", { articles: articles, categories: categories });//enviando p/ frontend
+        });
+    });
 });
+
+app.get("/:slug", (req, res) => {
+    var slug = req.params.slug;
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if (article != undefined) {
+            Category.findAll().then(categories => {
+                res.render("index", { article: article, categories: categories });
+            });
+        } else {
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/")
+    })
+});
+
+/*rota que filtra por categoria*/
+app.get("/category/:slug", (req, res) => {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{ model: Article }]
+    }).then(category => {
+        if (category != undefined) {
+            Category.findAll().then(categories => {
+                res.render("index", { articles: category.articles, categories: categories })
+            })
+        } else {
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    })
+})
 
 app.listen(8080, () => {
     console.log("O servidor está rodando!")
