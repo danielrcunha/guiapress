392b263edeb7e0d562afd70ff0f2961f9fc6a319
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2022-08-04 19:47:42 -0300

    Instalando bcryptjs
    Instalando bcryptjs e aplicando segurança ao salvar senhas de usuario

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require('bcryptjs');
 
 router.get("/admin/user", (req, res) => {
     res.send("Listagem de usuario");
@@ -8,6 +9,27 @@ router.get("/admin/user", (req, res) => {
 
 router.get("/admin/users/create", (req, res) => {
     res.render("admin/users/create");
+});
+
+/*ROTA P/ RECEBER CAD USUÁRIO TIPO POST*/
+router.post("/users/create", (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+
+    User.create({
+        email: email,
+        password: hash
+    }).then(() => {
+        res.redirect("/");
+    }).catch((err) => {
+        res.redirect("/");
+    });
+
+    //se quiser testar no JSON p/ aparecer na tela
+    //res.json({ email, password });
 });
 
 module.exports = router;
